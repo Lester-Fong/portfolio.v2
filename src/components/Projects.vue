5afe31354b6fd8fa9db2546cd0925a9e95e7b984
Lester Fong <lester.fong@switchconnect.com.au>
2025-02-11 18:43:06 +0800

    add featured projects section with detailed descriptions and links

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,276 @@
 <template>
   <section class="fng-projects">
     <h2 class="fng-projects-numbered-heading">Some things I've Built.</h2>
+
+    <!-- HAPPY KIDS -->
+    <div class="row d-flex justify-content-between align-items-center pb-5">
+      <div class="col-7">
+        <a href="javascript:void(0)" class="position-relative">
+          <div class="img-overlayer"></div>
+          <div class="w-100">
+            <img
+              src="/src/assets/happykids.png"
+              class="img-cover"
+              width="100%"
+              height="400"
+              alt="Happy Kids Photo"
+            />
+          </div>
+        </a>
+      </div>
+      <div class="col-5 text-end">
+        <div class="fng-projects-content">
+          <p>Featured Project</p>
+          <h3 class="fng-projects-content-title">
+            <a href="javascript:void(0)">Happy Kids</a>
+          </h3>
+          <div class="fng-projects-content-description">
+            <p>
+              A fundraising platform for children's health and education. This
+              web application has a Content Management System (CMS) with Paypal
+              Payment Gateway (API). It is used by a non-profit organization
+              from Trimex Colleges.
+            </p>
+          </div>
+          <ul class="fng-projects-content-tech-list">
+            <li>Vuejs</li>
+            <li>Laravel</li>
+            <li>Graphql</li>
+            <li>Mysql</li>
+            <li>Bootstrap</li>
+          </ul>
+          <div class="fng-projects-content-links">
+            <a
+              href="https://github.com/Lester-Fong/Happy-Kids"
+              aria-label="GitHub Link"
+              rel="noopener noreferrer"
+              target="_blank"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-github"
+              >
+                <title>GitHub</title>
+                <path
+                  d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                ></path></svg
+            ></a>
+
+            <a
+              href="javascript:void(0)"
+              aria-label="External Link"
+              class="external"
+              rel="noopener noreferrer"
+              target="_blank"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-external-link"
+              >
+                <title>External Link</title>
+                <path
+                  d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                ></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line></svg
+            ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- COMPANY ARTICLE PUBLISHER -->
+    <div
+      class="row d-flex justify-content-between align-items-center flex-row-reverse py-5"
+    >
+      <div class="col-7">
+        <a href="javascript:void(0)" class="position-relative">
+          <div class="img-overlayer"></div>
+          <div class="w-100">
+            <img
+              src="/src/assets/happykids.png"
+              class="img-cover"
+              width="100%"
+              height="400"
+              alt="Happy Kids Photo"
+            />
+          </div>
+        </a>
+      </div>
+      <div class="col-5 text-start">
+        <div class="fng-projects-content">
+          <p>Featured Project</p>
+          <h3 class="fng-projects-content-title">
+            <a href="javascript:void(0)">Company Article Publisher</a>
+          </h3>
+          <div class="fng-projects-content-description">
+            <p>
+              A role-based content management system built to facilitate writing
+              and editing articles within a company structure. The project
+              includes two roles: Writer and Editor. Writers are responsible for
+              drafting articles, while Editors can review, edit, and publish or
+              hide them. Editors also manage users and company details.
+            </p>
+          </div>
+          <ul class="fng-projects-content-tech-list justify-content-start">
+            <li class="ms-0 me-4">Vuejs</li>
+            <li class="ms-0 me-4">Laravel</li>
+            <li class="ms-0 me-4">Graphql</li>
+            <li class="ms-0 me-4">Mysql</li>
+            <li class="ms-0 me-4">Summernote</li>
+          </ul>
+          <div class="fng-projects-content-links justify-content-start">
+            <a
+              href="https://github.com/Lester-Fong/AI-FE-EXAM"
+              aria-label="GitHub Link"
+              rel="noopener noreferrer"
+              target="_blank"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-github"
+              >
+                <title>GitHub</title>
+                <path
+                  d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                ></path></svg
+            ></a>
+
+            <a
+              href="javascript:void(0)"
+              aria-label="External Link"
+              class="external"
+              rel="noopener noreferrer"
+              target="_blank"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-external-link"
+              >
+                <title>External Link</title>
+                <path
+                  d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                ></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line></svg
+            ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- RATE INFLUENCERS -->
+    <div class="row d-flex justify-content-between align-items-center py-5">
+      <div class="col-7">
+        <a href="javascript:void(0)" class="position-relative">
+          <div class="img-overlayer"></div>
+          <div class="w-100">
+            <img
+              src="/src/assets/rateinfluencers.png"
+              class="img-cover"
+              width="100%"
+              height="400"
+              alt="Happy Kids Photo"
+            />
+          </div>
+        </a>
+      </div>
+      <div class="col-5 text-end">
+        <div class="fng-projects-content">
+          <p>Featured Project</p>
+          <h3 class="fng-projects-content-title">
+            <a href="javascript:void(0)">Rate Influencer</a>
+          </h3>
+          <div class="fng-projects-content-description">
+            <p>
+              A platform where anyone can recommend and review famous
+              Influencers so that they can educate casual viewers which
+              Influencer is good or bad. Its goal is to help people make better
+              decisions when following an Influencer and to help Influencers
+              improve their content.
+            </p>
+          </div>
+          <ul class="fng-projects-content-tech-list">
+            <li>Vuejs</li>
+            <li>Laravel</li>
+            <li>Graphql</li>
+            <li>Mysql</li>
+            <li>Bootstrap</li>
+          </ul>
+          <div class="fng-projects-content-links">
+            <a
+              href="https://github.com/Lester-Fong/Rate-Influencers"
+              aria-label="GitHub Link"
+              rel="noopener noreferrer"
+              target="_blank"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-github"
+              >
+                <title>GitHub</title>
+                <path
+                  d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                ></path></svg
+            ></a>
+
+            <a
+              href="javascript:void(0)"
+              aria-label="External Link"
+              class="external"
+              rel="noopener noreferrer"
+              target="_blank"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-external-link"
+              >
+                <title>External Link</title>
+                <path
+                  d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                ></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line></svg
+            ></a>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
