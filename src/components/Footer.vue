44e2bc854b15d35ee1da659228a688544ccd6e06
Lester Fong <lester.fong@switchconnect.com.au>
2025-02-18 19:34:28 +0800

    add Footer component with attribution links

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="footer">
+    <a href="https://github.com/Lester-Fong/portfolio.v2">
+      <div>Built by: Lester Niel Fong</div>
+    </a>
+    <a href="https://brittanychiang.com/">
+      <div>Design from: Brittany Chiang</div>
+    </a>
+  </footer>
+</template>
