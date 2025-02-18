c9e6e39d7db0f515ffc52281ec2e1a67e0543c74
Lester Fong <lester.fong@switchconnect.com.au>
2025-02-18 19:34:12 +0800

    add Contact component with introductory message and button

---
+++ src/components/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="fng-contact">
+    <h2>What's Next?</h2>
+    <h1 class="fng-contact-title">Get in Touch.</h1>
+    <p>
+      <span>I am currently looking for new opportunities,</span> my inbox is
+      always open. Whether you have a question or just want to say hi, I'll try
+      my best to get back to you!
+    </p>
+
+    <button class="btn-default mt-5">Say Hello</button>
+  </section>
+</template>
