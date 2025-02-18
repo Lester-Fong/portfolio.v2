7c268cd10614c543c16f5127ebeaead5aa18d013
Lester Fong <lester.fong@switchconnect.com.au>
2025-02-18 19:35:02 +0800

    refactor IndexPage to use Noteworthy, Contact, and Footer components

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -41,95 +41,13 @@
     <Projects />
 
     <!-- Other Noteworthy Projects -->
-    <div class="fng-noteworthy">
-      <h2 class="fng-noteworthy-title">Other Noteworthy Projects</h2>
-      <a href="javascript:void(0)" class="fng-noteworthy-archive"
-        >view all archives</a
-      >
-
-      <ul class="fng-noteworthy-grid">
-        <li v-for="i in 6" :key="i">
-          <div class="fng-noteworthy-grid-inner">
-            <header class="w-100">
-              <div class="fng-noteworthy-grid-inner-top">
-                <div class="fng-noteworthy-grid-inner-top-folder">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="1"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-folder"
-                  >
-                    <title>Folder</title>
-                    <path
-                      d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"
-                    ></path>
-                  </svg>
-                </div>
-                <div class="fng-noteworthy-grid-inner-top-links">
-                  <a
-                    href="https://medium.com/stories-from-upstatement/integrating-algolia-search-with-wordpress-multisite-e2dea3ed449c"
-                    aria-label="External Link"
-                    class="external"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    ><svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      role="img"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-external-link"
-                    >
-                      <title>External Link</title>
-                      <path
-                        d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
-                      ></path>
-                      <polyline points="15 3 21 3 21 9"></polyline>
-                      <line x1="10" y1="14" x2="21" y2="3"></line></svg
-                  ></a>
-                </div>
-              </div>
-              <h3 class="fng-noteworthy-grid-inner-title">
-                <a
-                  href="https://medium.com/stories-from-upstatement/integrating-algolia-search-with-wordpress-multisite-e2dea3ed449c"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  >Sample Project 1</a
-                >
-              </h3>
-              <div class="fng-noteworthy-grid-inner-description">
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Veniam aut minus distinctio nobis illo cumque suscipit unde
-                  similique temporery.
-                </p>
-              </div>
-            </header>
-            <footer>
-              <ul class="fng-noteworthy-grid-inner-tech-list">
-                <li>Vuejs</li>
-                <li>Laravel</li>
-                <li>Graphql</li>
-              </ul>
-            </footer>
-          </div>
-        </li>
-      </ul>
-      <div class="mt-5">
-        <button class="btn-default">See More</button>
-      </div>
-    </div>
+    <Noteworthy />
 
     <!-------- Contact --------->
-    <section class="fng-contact"></section>
+    <Contact />
+
+    <!-------- Footer --------->
+    <Footer />
   </main>
 </template>
 
@@ -140,6 +58,9 @@ import RightEmail from "../components/RightEmail.vue";
 import Works from "@/components/Works.vue";
 import AboutMe from "@/components/AboutMe.vue";
 import Projects from "@/components/Projects.vue";
+import Noteworthy from "@/components/Noteworthy.vue";
+import Contact from "@/components/Contact.vue";
+import Footer from "@/components/Footer.vue";
 
 import { ref } from "vue";
 
